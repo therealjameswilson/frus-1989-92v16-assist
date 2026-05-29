67b2ded8efe4078158bc6103e13497a39a3d9342
therealjameswilson <30484292+therealjameswilson@users.noreply.github.com>
2026-05-29 07:02:50 -0400

    Create FRUS assist page

---
+++ data/volume.js
@@ -0,0 +1,12 @@
+window.FRUS_VOLUME = {
+  "index": 18,
+  "doc_id": "frus1989-92v16",
+  "title": "1989\u20131992, Volume XVI, Southeast Asia and the Pacific",
+  "official_url": "https://history.state.gov/historicaldocuments/frus1989-92v16",
+  "repo_slug": "frus-1989-92v16-assist",
+  "title_ascii": "1989-1992, Volume XVI, Southeast Asia and the Pacific",
+  "pages_url": "https://therealjameswilson.github.io/frus-1989-92v16-assist/",
+  "repo_url": "https://github.com/therealjameswilson/frus-1989-92v16-assist",
+  "nara_scout_url": "https://therealjameswilson.github.io/nara-scout/#q=XVI+OR+Southeast+OR+Asia+OR+Pacific&from=1989&to=1992&sort=relevance&perColl=25&perPage=50",
+  "companion": null
+};
